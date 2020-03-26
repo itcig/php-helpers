30dd0e78c8f45343916f94249a92a1628ea2cf4c
unfrgivn <bradley.ash@gmail.com>
2020-03-25 22:39:23 -0500

    Allow is_json to recieve invalid variables like non-strings and null and return false

--- src/functions/json.php
+++ src/functions/json.php
@@ -3,11 +3,15 @@
 namespace Cig;
 
 /**
- * @param string $json_string The JSON document
+ * @param mixed $json_string The JSON document
  *
  * @return bool True if valid JSON
  */
-function is_json(string $json_string): bool {
+function is_json($json_string): bool {
+	if (!is_string($json_string)) {
+		return false;
+	}
+
 	json_decode($json_string);
 	return (json_last_error() === JSON_ERROR_NONE);
 }
