f7c6952a90829d1e5d2745737f90dba5170fe853
unfrgivn <bradley.ash@gmail.com>
2020-05-20 13:33:55 -0500

    Added array_filter_recursive

--- src/functions/array.php
+++ src/functions/array.php
@@ -3,6 +3,25 @@
 namespace Cig;
 
 /**
+ * Recursively filter an array
+ *
+ * @param array $array
+ * @param callable $callback
+ *
+ * @return array
+ */
+function array_filter_recursive(array $array, callable $callback = null): array {
+	$array = is_callable($callback) ? array_filter($array, $callback) : array_filter($array);
+	foreach ($array as &$value) {
+		if (is_array($value)) {
+			$value = call_user_func(__FUNCTION__, $value, $callback);
+		}
+	}
+
+	return $array;
+}
+
+/**
  * Test if an array is associative
  *
  * @param array $arr
