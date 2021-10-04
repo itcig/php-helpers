7a1c7384da67d85b031315ef1a7a0a50525460ca
bree <groundcontroltomajorbree@gmail.com>
2021-10-04 16:32:35 -0500

    test: test dir setup

---
+++ tests/phpunit/Integration/BaseTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cig\Tests\Integration;
+
+use function Cig\auto_load_from_path;
+
+class BaseTestCase extends \Cig\PHPUnit\Unit\TestCase {
+
+	protected function setUp(): void {
+		parent::setUp();
+		auto_load_from_path(PHP_HELPERS_PATH . 'src/functions', PHP_HELPERS_PATH);
+	}
+}
---
+++ tests/phpunit/Unit/ExampleTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Cig\Tests\Unit;
+
+class ExampleTest extends \Cig\Tests\Unit\BaseTestCase {
+	protected function setUp(): void {
+		parent::setUp();
+	}
+}
