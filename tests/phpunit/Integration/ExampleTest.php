5420b8d1b2eec56ec68a4917723dc84d1c4c0aff
bree <34174060+juniormince@users.noreply.github.com>
2021-10-06 12:01:21 -0500

    test: phpunit & testing config setup (#10)

---
+++ tests/phpunit/Integration/ExampleTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Cig\Tests\Integration;
+
+class ExampleTest extends \Cig\Tests\Integration\BaseTestCase {
+	protected function setUp(): void {
+		parent::setUp();
+	}
+}
---
+++ tests/phpunit/autoload.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once(PHP_HELPERS_PATH . 'vendor/autoload.php');
+require_once(PHP_HELPERS_PATH . 'src/functions/autoload.php');
