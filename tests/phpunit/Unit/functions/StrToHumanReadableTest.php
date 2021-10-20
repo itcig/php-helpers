95419c3c3b94aa084f013d1a85baf8fdf1fdbd8f
bree <groundcontroltomajorbree@gmail.com>
2021-10-20 12:44:15 -0500

    test: bicapital to human readable

--- tests/phpunit/Unit/functions/StrToHumanReadableTest.php
+++ tests/phpunit/Unit/functions/StrToHumanReadableTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\Error\Notice;
 use PHPUnit\Framework\Error\Warning;
 
 class StrToHumanReadableTest extends \Cig\Tests\Unit\BaseTestCase {
+
 	//TODO: look into method re:what defines human readable, caps/all lowercase/etc
 	public function test_str_to_human_readable_camel(): void {
 		$string = "camelCaseStringTest!!";
@@ -13,6 +14,16 @@ class StrToHumanReadableTest extends \Cig\Tests\Unit\BaseTestCase {
 
 		$result = \Cig\str_to_human_readable($string);
 
+		self::assertSame($expected_result, $result);
+	}
+
+	public function test_str_to_human_readable_bicapital(): void {
+		$string = "danny-DeVito_";
+		$expected_result = "danny De Vito";
+
+		$result = \Cig\str_to_human_readable($string);
+
+		// note: not technically a 'fail' but incorrectly separates surnames with bicapital
 		self::assertSame($expected_result, $result);
 	}
 }
