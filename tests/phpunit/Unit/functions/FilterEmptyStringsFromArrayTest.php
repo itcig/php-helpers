21eaed4836525339a029f23cf0f3c1310d1424b1
bree <groundcontroltomajorbree@gmail.com>
2021-10-28 16:32:22 -0500

    test: mixed array + comments

--- tests/phpunit/Unit/functions/FilterEmptyStringsFromArrayTest.php
+++ tests/phpunit/Unit/functions/FilterEmptyStringsFromArrayTest.php
@@ -23,7 +23,7 @@ class FilterEmptyStringsFromArrayTest extends \Cig\Tests\Unit\BaseTestCase {
 	/**
 	 * @covers ::\Cig\filter_empty_strings_from_array
 	 *
-	 * @param $string
+	 * @param $array
 	 * @param $expected_result
 	 */
 	public function test_filter_empty_strings_from_assoc_array(): void {
@@ -32,7 +32,21 @@ class FilterEmptyStringsFromArrayTest extends \Cig\Tests\Unit\BaseTestCase {
 
 		$result = \Cig\filter_empty_strings_from_array($array);
 
-		var_dump($result);
+		self::assertIsArray($result);
+		self::assertSame($expected_result, $result);
+	}
+
+	/**
+	 * @covers ::\Cig\filter_empty_strings_from_array
+	 *
+	 * @param $array
+	 * @param $expected_result
+	 */
+	public function test_filter_empty_strings_from_mixed_array(): void {
+		$array = [0 => 'full moon', 1 => '', 'mummy' => '', 'dracula' => 'castle'];
+		$expected_result = [0 => 'full moon', 'dracula' => 'castle'];
+
+		$result = \Cig\filter_empty_strings_from_array($array);
 
 		self::assertIsArray($result);
 		self::assertSame($expected_result, $result);
