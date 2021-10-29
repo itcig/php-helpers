a21f4293800a264d6c88966dfd42f25b885b498f
bree <groundcontroltomajorbree@gmail.com>
2021-10-29 17:34:42 -0500

    test: dataProvider test

--- tests/phpunit/Unit/functions/FilterEmptyStringsFromArrayTest.php
+++ tests/phpunit/Unit/functions/FilterEmptyStringsFromArrayTest.php
@@ -5,14 +5,39 @@ namespace Cig\Tests\Unit\Functions;
 class FilterEmptyStringsFromArrayTest extends \Cig\Tests\Unit\BaseTestCase {
 
 	/**
+	 * @return array[]
+	 */
+	public function provide_empty_string_array_data(): array {
+		return [
+			'non_assoc' => [
+				//array
+				['dracula', '', 'werewolf'],
+				//expected result
+				[0 => 'dracula', 2 => 'werewolf'],
+			],
+//			'assoc' => [
+//				//array
+//
+//				//expected result
+//			],
+//			'mixed_array' => [
+//				//array
+//
+//				//expected result
+//			],
+		];
+	}
+
+	/**
 	 * @covers ::\Cig\filter_empty_strings_from_array
 	 *
+	 * @dataProvider provide_empty_string_array_data
 	 * @param $array
 	 * @param $expected_result
 	 */
-	public function test_filter_empty_strings_from_non_assoc_array(): void {
-		$array = ['dracula', '', 'werewolf'];
-		$expected_result = [0 => 'dracula', 2 => 'werewolf'];
+	public function test_filter_empty_strings_from_non_assoc_array($array, $expected_result): void {
+//		$array = ['dracula', '', 'werewolf'];
+//		$expected_result = [0 => 'dracula', 2 => 'werewolf'];
 
 		$result = \Cig\filter_empty_strings_from_array($array);
 
