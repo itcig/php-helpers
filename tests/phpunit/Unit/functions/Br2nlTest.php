0003b92fef5d14743e8fe37baa7a06c299e64ed5
bree <groundcontroltomajorbree@gmail.com>
2021-11-04 17:13:19 -0500

    test: add no space no whack + cover comments

--- tests/phpunit/Unit/functions/Br2nlTest.php
+++ tests/phpunit/Unit/functions/Br2nlTest.php
@@ -6,6 +6,9 @@ namespace Cig\Tests\Unit\Functions;
 
 class Br2nlTest extends \Cig\Tests\Unit\BaseTestCase {
 
+	/**
+	 * @covers ::\Cig\br2nl()
+	 */
 	public function test_br2nl(): void {
 		$string = 'here is a <br />line break';
 		$expected_result = 'here is a 
@@ -17,9 +20,27 @@ line break';
 		self::assertSame($expected_result, $result);
 	}
 
-	public function test_br_no_whack(): void {
-		$string = "here is a <br>line break";
+	/**
+	 * test <br/> with no space
+	 * @covers ::\Cig\br2nl()
+	 */
+	public function test_br_no_space(): void {
+		$string = "here is a <br/>line break";
 //		$expected_result = 'here is a
+//line break';
+
+		$result = \Cig\br2nl($string);
+
+		self::markTestSkipped('**WIP** Revisit this test - does not consider br without space');
+	}
+
+	/**
+	 * test <br > with no whack
+	 * @covers ::\Cig\br2nl()
+	 */
+	public function test_br_no_whack(): void {
+		$string = "here is a <br >line break";
+		//		$expected_result = 'here is a
 //line break';
 
 		$result = \Cig\br2nl($string);
@@ -27,9 +48,13 @@ line break';
 		self::markTestSkipped('**WIP** Revisit this test — does not consider br without whack');
 	}
 
-	public function test_br_no_space(): void {
+	/**
+	 * test <br> with no space or whack
+	 * @covers ::\Cig\br2nl()
+	 */
+	public function test_br_no_space_no_whack(): void {
 		$string = "here is a <br>line break";
-//		$expected_result = 'here is a
+		//		$expected_result = 'here is a
 //line break';
 
 		$result = \Cig\br2nl($string);
