8a7d70372633a9bfe4c79ea39fe8aa33f59d1ab2
bree <groundcontroltomajorbree@gmail.com>
2021-11-04 16:15:37 -0500

    style: rm wip dataProvider

--- tests/phpunit/Unit/functions/StrToWordsTest.php
+++ tests/phpunit/Unit/functions/StrToWordsTest.php
@@ -2,133 +2,119 @@
 
 namespace Cig\Tests\Unit\Functions;
 
-//TODO: string helpers are really stepping on each other in string.php
-
 class StrToWordsTest extends \Cig\Tests\Unit\BaseTestCase {
 
-	public function provide_str_to_words_data(): array {
-		return [
-			// dataProvider to come
+	public function test_str_to_words(): void {
+		$string = 'ConvertCamelCaseToMultipleWords';
+		$expected_result = [
+			'Convert',
+			'Camel',
+			'Case',
+			'To',
+			'Multiple',
+			'Words',
 		];
+
+		$result = \Cig\str_to_words($string);
+
+		self::assertIsArray($result);
+		self::assertSame($expected_result, $result);
 	}
 
-public
-function test_str_to_words(): void {
-	$string = 'ConvertCamelCaseToMultipleWords';
-	$expected_result = [
-		'Convert',
-		'Camel',
-		'Case',
-		'To',
-		'Multiple',
-		'Words',
-	];
+	public function test_str_to_words_lowercase(): void {
+		$string = 'What We Do In The Shadows';
+		$lower = FALSE;
+		$expected_result = [
+			'What',
+			'We',
+			'Do',
+			'In',
+			'The',
+			'Shadows',
+		];
 
-	$result = \Cig\str_to_words($string);
+		$result = \Cig\str_to_words($string, $lower);
 
-	self::assertIsArray($result);
-	self::assertSame($expected_result, $result);
-}
+		self::assertIsArray($result);
+		self::assertSame($expected_result, $result);
 
-public
-function test_str_to_words_lowercase(): void {
-	$string = 'What We Do In The Shadows';
-	$lower = FALSE;
-	$expected_result = [
-		'What',
-		'We',
-		'Do',
-		'In',
-		'The',
-		'Shadows',
-	];
+	}
 
-	$result = \Cig\str_to_words($string, $lower);
+	public function test_str_to_words_not_lowercase(): void {
+		$string = 'What We Do In The Shadows';
+		$lower = TRUE;
+		$expected_result = [
+			'what',
+			'we',
+			'do',
+			'in',
+			'the',
+			'shadows',
+		];
 
-	self::assertIsArray($result);
-	self::assertSame($expected_result, $result);
+		$result = \Cig\str_to_words($string, $lower);
 
-}
+		self::assertIsArray($result);
+		self::assertSame($expected_result, $result);
+	}
 
-public
-function test_str_to_words_not_lowercase(): void {
-	$string = 'What We Do In The Shadows';
-	$lower = TRUE;
-	$expected_result = [
-		'what',
-		'we',
-		'do',
-		'in',
-		'the',
-		'shadows',
-	];
+	public function test_str_to_words_no_punctuation(): void {
+		$string = 'What?? _ Are-we-doing. In the shadows.';
+		$lower = FALSE; //default
+		$remove_punctuation = TRUE;
 
-	$result = \Cig\str_to_words($string, $lower);
+		$expected_result = [
+			'What',
+			'Are',
+			'we',
+			'doing',
+			'In',
+			'the',
+			'shadows',
+		];
 
-	self::assertIsArray($result);
-	self::assertSame($expected_result, $result);
-}
+		$result = \Cig\str_to_words($string, $lower, $remove_punctuation);
 
-public
-function test_str_to_words_no_punctuation(): void {
-	$string = 'What?? _ Are-we-doing. In the shadows.';
-	$lower = FALSE; //default
-	$remove_punctuation = TRUE;
+		self::assertIsArray($result);
+		self::assertSame($expected_result, $result);
+	}
 
-	$expected_result = [
-		'What',
-		'Are',
-		'we',
-		'doing',
-		'In',
-		'the',
-		'shadows',
-	];
+	public function test_str_to_words_punctuation(): void {
+		$string = 'What?? _Are-we-doing. In the shadows.';
+		$lower = FALSE; //default
+		$remove_punctuation = FALSE;
 
-	$result = \Cig\str_to_words($string, $lower, $remove_punctuation);
+		$expected_result = [
+			'What??',
+			'_',
+			'Are-we-doing.',
+			'In',
+			'the',
+			'shadows.',
+		];
+		$result = \Cig\str_to_words($string);
 
-	self::assertIsArray($result);
-	self::assertSame($expected_result, $result);
-}
+		self::assertIsArray($result);
+		// self::assertSame($expected_result, $result);
+	}
 
-public
-function test_str_to_words_punctuation(): void {
-	$string = 'What?? _Are-we-doing. In the shadows.';
-	$lower = FALSE; //default
-	$remove_punctuation = FALSE;
+	public function test_str_to_words_question_mark(): void {
+		$string = 'What?? Are we doing. In the shadows.';
+		$lower = FALSE; //default
+		$remove_punctuation = FALSE;
 
-	$expected_result = [
-		'What??',
-		'_',
-		'Are-we-doing.',
-		'In',
-		'the',
-		'shadows.',
-	];
-	$result = \Cig\str_to_words($string);
+		//		$expected_result = [
+		//			'What??',
+		//			'Are',
+		//			'we',
+		//			'doing.',
+		//			'In',
+		//			'the',
+		//			'shadows.',
+		//		];
 
-	self::assertIsArray($result);
-	// self::assertSame($expected_result, $result);
-}
+		$result = \Cig\str_to_words($string);
 
-public
-function test_str_to_words_question_mark(): void {
-	$string = 'What?? Are we doing. In the shadows.';
-	$lower = FALSE; //default
-	$remove_punctuation = FALSE;
-
-	//		$expected_result = [
-	//			'What??',
-	//			'Are',
-	//			'we',
-	//			'doing.',
-	//			'In',
-	//			'the',
-	//			'shadows.',
-	//		];
-
-	$result = \Cig\str_to_words($string);
-
-	self::markTestSkipped('**WIP** Revisit this test — Question marks are punctuation but always treated under regex and thus always removed in spite of argument');
-}
+		self::markTestSkipped('**WIP** Revisit this test — Question marks are punctuation but always treated under regex and thus always removed in spite of argument');
+	}
 }
