1bc1b131a855a466201e313c0a588809ef7a0721
bree <groundcontroltomajorbree@gmail.com>
2021-11-18 21:44:17 -0600

    test: add skips

---
+++ tests/phpunit/Unit/functions/AutoLoadFromPathTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Cig\Tests\Unit\Functions;
+
+class AutoLoadFromPathTest extends \Cig\Tests\Unit\BaseTestCase {
+
+	/**
+	 * @covers ::\Cig\auto_load_from_path()
+	 */
+	public function test_auto_load_from_path(): void {
+
+//		$result = \Cig\auto_load_from_path();
+
+		self::markTestSkipped('**WIP** Revisit');
+	}
+}
