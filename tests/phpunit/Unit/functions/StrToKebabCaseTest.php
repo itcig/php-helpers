67bd42ad8b9052a93b9b8e4684777b2bccd45b4e
bree <groundcontroltomajorbree@gmail.com>
2021-11-19 00:43:45 -0600

    test: strict typing fix

--- tests/phpunit/Unit/functions/StrToKebabCaseTest.php
+++ tests/phpunit/Unit/functions/StrToKebabCaseTest.php
@@ -1,9 +1,12 @@
 <?php
+declare(strict_types=1);
 
 namespace Cig\Tests\Unit\Functions;
 
 
 class StrToKebabCaseTest extends \Cig\Tests\Unit\BaseTestCase {
+
+//	TODO: stub str_to_words
 
 	/**
 	 * @covers ::\Cig\str_to_kebab_case()
@@ -19,8 +22,6 @@ class StrToKebabCaseTest extends \Cig\Tests\Unit\BaseTestCase {
 		self::assertSame($expected_result, $result);
 	}
 
-	//TODO: expected this to break but this method goes ahead and turns
-	// numbers into strings despite typescript asking for a string
 	public function test_number_to_kebab_case(): void {
 		$not_a_string = 1011;
 		// $expected_result = "don't expect result";
@@ -28,9 +29,9 @@ class StrToKebabCaseTest extends \Cig\Tests\Unit\BaseTestCase {
 		// TODO: showing test with string result to document. correct or refactor?
 		$method_result = '1011';
 
-		$result = \Cig\str_to_kebab_case($not_a_string);
+		self::expectError();
+		self::expectErrorMessage('Argument 1 passed to Cig\str_to_kebab_case() must be of the type string, int given');
 
-		self::assertIsString($result);
-		self::assertSame($method_result, $result);
+		$result = \Cig\str_to_kebab_case($not_a_string);
 	}
 }
