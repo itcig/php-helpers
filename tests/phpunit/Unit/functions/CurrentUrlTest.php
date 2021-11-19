0aca580ee2c4a500f2850ecf5cf6004b039b5d20
bree <groundcontroltomajorbree@gmail.com>
2021-11-18 22:04:47 -0600

    test: add skips

---
+++ tests/phpunit/Unit/functions/CurrentUrlTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Cig\Tests\Unit\Functions;
+
+class CurrentUrlTest extends \Cig\Tests\Unit\BaseTestCase {
+
+	/**
+	 * @covers ::\Cig\current_url()
+	 */
+	public function test_current_url(): void {
+
+//		$result = \Cig\current_url();
+
+		self::markTestSkipped('**WIP** Revisit');
+	}
+}
